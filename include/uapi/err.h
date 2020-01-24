e6fbc060ab82078be4b42f9189879adb4abaf5af
Yizhou Shan <syzwhat@gmail.com>
2020-01-24 11:35:59 -0800

    Add few generic headers

---
+++ include/uapi/err.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2020 Wuklab, UCSD. All rights reserved.
+ */
+
+#ifndef _LEGOMEM_UAPI_ERR_H_
+#define _LEGOMEM_UAPI_ERR_H_
+
+#include <uapi/compiler.h>
+
+/*
+ * The address of a block returned by malloc or realloc in GNU
+ * systems is always a multiple of eight (or sixteen on 64-bit systems).
+ *
+ * Thus we may encode error number in low bits.
+ */
+#define MAX_ERRNO	4095
+
+#define IS_ERR_VALUE(x) unlikely((x) >= (unsigned long)-MAX_ERRNO)
+
+static inline void *ERR_PTR(long error)
+{
+	return (void *)error;
+}
+
+static inline long PTR_ERR(const void *ptr)
+{
+	return (long)ptr;
+}
+
+static inline long IS_ERR(const void *ptr)
+{
+	return IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline long IS_ERR_OR_NULL(const void *ptr)
+{
+	return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline void *ERR_CAST(const void *ptr)
+{
+	/* cast away the const */
+	return (void *)ptr;
+}
+
+static inline int PTR_RET(const void *ptr)
+{
+	if (IS_ERR(ptr))
+		return PTR_ERR(ptr);
+	else
+		return 0;
+}
+
+#endif /* _LEGOMEM_UAPI_ERR_H_ */
