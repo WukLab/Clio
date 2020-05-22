2ea0acf177590edce880458e7efc5e4cddc8de36
root <root@wuklab02.ecn.purdue.edu>
2020-05-22 03:08:01 -0400

    small changes

--- host/api_kvs.h
+++ host/api_kvs.h
@@ -7,11 +7,12 @@
 #define MAX_KEY_SIZE 8
 
 int legomem_kvs_create(struct legomem_context *ctx, uint16_t key_size,
-		      uint64_t key, uint16_t value_size, void *value);
+		      char *key, uint16_t value_size, void *value);
 int legomem_kvs_update(struct legomem_context *ctx, uint16_t key_size,
-		       uint64_t key, uint16_t value_size, void *value);
+		       char *key, uint16_t value_size, void *value);
 int legomem_kvs_read(struct legomem_context *ctx, uint16_t key_size,
-		     uint64_t key, uint16_t value_size, void *value);
-int legomem_kvs_delete(struct legomem_context *ctx, uint16_t key_size, uint64_t key);
+		     char *key, uint16_t value_size, void *value);
+int legomem_kvs_delete(struct legomem_context *ctx, uint16_t key_size, 
+			char *key);
 
 #endif /* _HOST_API_KVS_H_ */
