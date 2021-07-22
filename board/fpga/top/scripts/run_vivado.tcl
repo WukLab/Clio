59163d9f8776b92b81e27b0bc8f13a4d629b7e5f
Yizhou Shan <syzwhat@gmail.com>
2021-07-22 01:13:26 -0700

    Add fpga build flow

--- board/fpga/top/scripts/run_vivado.tcl
+++ board/fpga/top/scripts/run_vivado.tcl
@@ -80,8 +80,10 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 set obj [get_filesets sources_1]
 set_property "ip_repo_paths" "
 	[file normalize "$origin_dir/../generated_ip"]
-	[file normalize "/data/ip/legomem"]
 	" $obj
+
+#	[file normalize "/data/ip/legomem"]
+
 update_ip_catalog -rebuild
 
 # Set 'sources_1' fileset object
